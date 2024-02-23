2d7ec510eb83d30c21213c550c2e5e4739a6076c
Dohangang <121237964+Dohangang@users.noreply.github.com>
2024-02-23 14:20:48 +0900

    디버깅용 서버

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('')
 export class AppController {
-  @Get()
-  home() {
-    return 'Welcome to my Movie API';
+  @Get(':n/:m')
+  home(@Param('n') n: number, @Param('m') m: number) {
+    function gcd(a, b) {
+      return b ? gcd(b, a % b) : a;
+    }
+
+    function lcm(a, b) {
+      return (a * b) / gcd(a, b);
+    }
+
+    return [gcd(n, m), lcm(n, m)];
   }
 }
